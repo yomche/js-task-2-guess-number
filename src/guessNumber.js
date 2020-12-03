a5eaed12d568325cefc0a0f833135ee2a627ba86
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2020-12-03 18:55:43 +0300

    change __proto__ method to setPrototypeOf method

--- src/guessNumber.js
+++ src/guessNumber.js
@@ -5,13 +5,12 @@ const generateRandomNumber = {
 };
 
 const guessNumber = {
-    __proto__: generateRandomNumber,
+//    __proto__: generateRandomNumber,
     guess(userNumber) {
         if (userNumber > this.randomNumber) {
             io.write('Your number is bigger than guessed. Try again');
         } else if (userNumber < this.randomNumber) {
             io.write('Your number is smaller than guessed. Try again');
-            // eslint-disable-next-line
         } else if (userNumber == this.randomNumber) {
             io.write('You guessed right! Congratz!');
             return true;
@@ -21,15 +20,15 @@ const guessNumber = {
         return false;
     }
 };
+// назначает базовый объект generateRandomNumber как протитип для объекта guessNumber
+Object.setPrototypeOf(guessNumber, generateRandomNumber);
 
 const guessGame = {
     async main() {
         io.write('Try to guess number. Input yours: ');
-        // eslint-disable-next-line no-plusplus
         for (let attemptsCounter = 3; attemptsCounter > 0; --attemptsCounter) {
             // eslint-disable-next-line no-await-in-loop
             const number = await io.read();
-            // eslint-disable-next-line radix
             if (guessNumber.guess(number)) return;
         }
         io.write('');
