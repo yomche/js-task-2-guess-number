308e590d80035cc8ea75410b1fba47dc08ad19c5
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2020-12-04 01:19:21 +0300

    change "if statement" to "switch statement"

--- src/guessNumber.js
+++ src/guessNumber.js
@@ -1,41 +1,42 @@
 const io = require('console-read-write');
 
-const generateRandomNumber = {
-    randomNumber: Math.round(Math.random() * 10 + 1)
+const randomNumber = {
+    generateRandomNumber: Math.round(Math.random() * 10 + 1),
 };
 
 const guessNumber = {
-//    __proto__: generateRandomNumber,
     guess(userNumber) {
-        if (userNumber > this.randomNumber) {
-            io.write('Your number is bigger than guessed. Try again');
-        } else if (userNumber < this.randomNumber) {
-            io.write('Your number is smaller than guessed. Try again');
-        } else if (userNumber == this.randomNumber) {
-            io.write('You guessed right! Congratz!');
-            return true;
-        } else {
-            io.write('This is not a number');
+        switch (true) {
+            case userNumber > this.generateRandomNumber:
+                io.write('Your number is bigger than guessed. Try again');
+                break;
+            case userNumber < this.generateRandomNumber:
+                io.write('Your number is smaller than guessed. Try again');
+                break;
+            case userNumber == this.generateRandomNumber:
+                io.write('You guessed right! Congratz!');
+                return true;
+            default:
+                io.write('This is not a number');
+                break;
         }
-        return false;
-    }
+    },
 };
-// назначает базовый объект generateRandomNumber как протитип для объекта guessNumber
-Object.setPrototypeOf(guessNumber, generateRandomNumber);
+
+Object.setPrototypeOf(guessNumber, randomNumber);
 
 const guessGame = {
     async main() {
         io.write('Try to guess number. Input yours: ');
         for (let attemptsCounter = 3; attemptsCounter > 0; --attemptsCounter) {
-            // eslint-disable-next-line no-await-in-loop
             const number = await io.read();
             if (guessNumber.guess(number)) return;
         }
         io.write('');
         io.write('');
         io.write('Amount of attempts is ended');
-        io.write(`Guessed number is ${generateRandomNumber.randomNumber}`);
-    }
+        io.write(`Guessed number is ${randomNumber.generateRandomNumber}`);
+    },
 };
 
 guessGame.main();
